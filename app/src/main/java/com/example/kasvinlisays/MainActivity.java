c4b5c9138b9ef00d582ccbb35dd66e951e9b0b20
rojoslavik <36035119+rojoslavik@users.noreply.github.com>
2019-03-29 13:04:33 +0200

    Toiminnallisuusksien tekoa, kuvan otto toimii, tallennus ei.

--- app/src/main/java/com/example/kasvinlisays/MainActivity.java
+++ app/src/main/java/com/example/kasvinlisays/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.kasvinlisays;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.os.Environment;
 import android.provider.MediaStore;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -10,30 +12,21 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.net.Uri;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 
 public class MainActivity extends AppCompatActivity {
 
+    private String currentPhotoPath;
     static final int REQUEST_IMAGE_CAPTURE = 1;
     String kasvinLaji;
     String kasviNimi;
-
-    private void dispatchPictureIntent() {
-        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
-        }
-    }
-
- /*   @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-            Bundle extras = data.getExtras();
-            Bitmap imageBitmap = (Bitmap) extras.get("data");
-            kasvinKuva.setImageBitmap(imageBitmap);
-        }
-    }
-    */
+    private ImageView image;
 
 
 
@@ -50,6 +43,8 @@ public class MainActivity extends AppCompatActivity {
         kasviNimi = nimi.getText().toString();
         final TextView kasviLaji = findViewById(R.id.kasviLaji);
 
+        image = findViewById(R.id.kasvinKuva);
+
 
         lajinLisays.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -62,7 +57,9 @@ public class MainActivity extends AppCompatActivity {
         otaKuva.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                dispatchPictureIntent();
+
+          dispatchTakePictureIntent();
+
 
             }
 
@@ -75,5 +72,49 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+    }
+
+
+    private void dispatchTakePictureIntent() {
+        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (cameraIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(cameraIntent, REQUEST_IMAGE_CAPTURE);
+
+            File pictureFile = null;
+            try {
+                pictureFile = createImageFile();
+            } catch (IOException ex) {
+                Toast.makeText(this,
+                        "Photo file can't be created, please try again",
+                        Toast.LENGTH_SHORT).show();
+                return;
+            }
+            if (pictureFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        "com.example.android.fileprovider",
+                        pictureFile);
+                cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(cameraIntent, REQUEST_IMAGE_CAPTURE);
+            }
+        }
+    }
+
+    private File createImageFile() throws IOException {
+        String timeStamp = new SimpleDateFormat("yyyyMMddHHmmss").format(new Date());
+        String pictureFile = "PLANT_" + timeStamp;
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(pictureFile,  ".jpg", storageDir);
+        currentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            File imgFile = new  File(currentPhotoPath);
+            if(imgFile.exists())            {
+                image.setImageURI(Uri.fromFile(imgFile));
+            }
+        }
     }
 }
